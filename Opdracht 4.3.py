6f19850beface577d3ce3272dc7d7ddac2360ed7
JaimeyHolm <jaimey.holm@student.hu.nl>
2018-09-18 09:41:17 +0200

    Opdracht 4.3

---
+++ Opdracht 4.3.py
@@ -0,0 +1,5 @@
+uurloon  = float(input('Wat verdien je per uur: '))
+aantalUur = int(input('Hoeveel uur heb je gewerkt: '))
+salaris = (uurloon * aantalUur)
+zin = str(aantalUur) + ' uur werken levert ' + str(salaris) + ' Euro op'
+print(zin)
