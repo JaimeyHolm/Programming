48b2f4cad30d59dc2e5ec116d44227ca496d52c6
JaimeyHolm <jaimey.holm@student.hu.nl>
2018-09-24 11:47:10 +0200

    Opdracht 6.6

---
+++ Opdracht 6.6.py
@@ -0,0 +1,11 @@
+def wijzig(letterlijst):
+    lijst = letterlijst
+    letterlijst[0] = 'd'
+    letterlijst[1] = 'e'
+    letterlijst[2] = 'f'
+
+lijst = ['a', 'b', 'c']
+
+print(lijst)
+wijzig(lijst)
+print(lijst)
