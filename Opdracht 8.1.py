40d6e1ad5b74133a2fdbb956720ebe0dddfc1a5a
JaimeyHolm <jaimey.holm@student.hu.nl>
2018-09-27 17:57:49 +0200

    Opdracht 8.1

---
+++ Opdracht 8.1.py
@@ -0,0 +1,14 @@
+def seizoen(maand):
+    if maand == 12 or maand == 1 or maand == 2:
+        return('Winter')
+    elif maand == 3 or maand == 4 or maand == 5:
+        return('Lente')
+    elif maand == 6 or maand == 7 or maand == 8:
+        return('Zomer')
+    elif maand == 9 or maand == 10 or maand == 11:
+        return('Herfst')
+
+
+maand = int(input('Hoeveelste maand is het? '))
+
+print(seizoen(maand))
