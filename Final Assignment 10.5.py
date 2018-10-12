b4870e05dc9422298459ca07d496e44416591604
JaimeyHolm <jaimey.holm@student.hu.nl>
2018-10-12 20:10:06 +0200

    Final Assignment 10.5

--- Final Assignment 10.5.py
+++ Final Assignment 10.5.py
@@ -4,17 +4,17 @@ def inlezen_beginstation(stations):
     beginStation = input('Wat is uw beginstation? ')
     while beginStation not in stations:
         print('Deze trein komt niet in ' + beginStation + '\n')
-        beginStation = input('Begin? ')
+        beginStation = input('Wat is uw beginstation? ')
     return beginStation
 
 def inlezen_eindstation(stations, beginStation):
     eindStation = input('Wat is uw eindstation? ')
     while eindStation not in stations:
-        print('Deze trein komt niet in ' + eindStation + '\n')
+        print('Deze trein komt niet in ' + eindStation + '.\n')
         eindStation = input('Wat is uw eindstation? ')
 
     while stations.index(eindStation) < stations.index(beginStation):
-        print('Met deze trein komt u niet in ' + eindStation + '\n')
+        print('Met deze trein komt u niet in ' + eindStation + '.\n')
         eindStation = input('Wat is uw eindstation? ')
 
     return eindStation
@@ -24,6 +24,10 @@ def omroepen_reis(stations, beginStation, eindStation):
     print('Het eindstation', eindStation, 'is het', (stations.index(eindStation) + 1), 'e station in het traject.')
     print('De afstand bedraagt', (stations.index(eindStation) + 1) - (stations.index(beginStation) + 1), 'station(s).')
     print('De prijs van het kaartje is', (((stations.index(eindStation) + 1) - (stations.index(beginStation) + 1)) * 5), 'euro.')
+    print('U stapt in de trein in:', beginStation)
+    for station in range(int(stations.index(beginStation) + 1), (int(stations.index(eindStation)))):
+        print(' - '+(stations[station]))
+    print('U stapt uit in: ' + eindStation)
 
 stations = ("Schagen", "Heerhugowaard", "Alkmaar", "Castricum", "Zaandam", "Amsterdam Sloterdijk", "Amsterdam Centraal", "Amsterdam Amstel", "Utrecht Centraal", "’s-Hertogenbosch", "Eindhoven", "Weert", "Roermond", "Sittard", "Maastricht")
 
